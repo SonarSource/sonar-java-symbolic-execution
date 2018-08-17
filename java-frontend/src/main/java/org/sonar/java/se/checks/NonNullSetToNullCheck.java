SonarSource/sonar-java-symbolic-execution

commit cd8cae46d5da7381683d55caf63eca91d2ef2ff0

SONARJAVA-2839 S2637 Fix FP in private constructor

java-frontend/src/main/java/org/sonar/java/se/checks/NonNullSetToNullCheck.java
@@ -21,6 +21,8 @@ package org.sonar.java.se.checks;
 
 import com.google.common.collect.Lists;
 import java.text.MessageFormat;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
 import java.util.stream.Stream;
 import javax.annotation.Nullable;
@@ -61,11 +63,16 @@ public class NonNullSetToNullCheck extends SECheck {
     "javax.persistence.MappedSuperclass"
   };
 
-  private MethodTree methodTree;
+  private Deque<MethodTree> methodTrees = new ArrayDeque<>();
 
   @Override
   public void init(MethodTree tree, CFG cfg) {
-    methodTree = tree;
+    methodTrees.push(tree);
+  }
+
+  @Override
+  public void checkEndOfExecution(CheckerContext context) {
+    methodTrees.pop();
   }
 
   @Override
@@ -84,6 +91,7 @@ public class NonNullSetToNullCheck extends SECheck {
 
   @Override
   public void checkEndOfExecutionPath(CheckerContext context, ConstraintManager constraintManager) {
+    MethodTree methodTree = methodTrees.peek();
     if (methodTree.is(Tree.Kind.CONSTRUCTOR)
       && !isDefaultConstructorForJpa(methodTree)
       && !callsThisConstructor(methodTree)

java-frontend/src/test/files/se/NonNullSetToNullCheck.java
@@ -329,3 +329,19 @@ abstract class ExcludedMethods {
     com.google.common.base.Preconditions.checkNotNull(getMyObject(), "yolo", new Object(), 2); // Compliant
   }
 }
+
+
+public class MultipleConstructors {
+
+  @Nonnull
+  private final String a;
+
+  MultipleConstructors() {
+    this("test");
+  }
+
+  private MultipleConstructors(@Nonnull String b) {
+    this.a = b;
+  }
+
+}

commit 3afd563fd7002f497adc40a41c963373af16389d

SONARJAVA-2839 Fix memory leak introduced in cd8cae4 (#2188)

java-frontend/src/main/java/org/sonar/java/se/checks/NonNullSetToNullCheck.java
@@ -76,6 +76,11 @@ public class NonNullSetToNullCheck extends SECheck {
   }
 
   @Override
+  public void interruptedExecution(CheckerContext context) {
+    methodTrees.pop();
+  }
+
+  @Override
   public ProgramState checkPreStatement(CheckerContext context, Tree syntaxNode) {
     AbstractStatementVisitor visitor = new PreStatementVisitor(context);
     syntaxNode.accept(visitor);