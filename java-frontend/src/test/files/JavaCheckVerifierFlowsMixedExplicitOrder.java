39f99d95b75e0ee67d47228c0fc6fa43cbe590e2
margarita-nedzelska-sonarsource <70522623+margarita-nedzelska-sonarsource@users.noreply.github.com>
2021-02-19 17:14:30 +0100

    SONARJAVA-3698 Extract Check Verifier from "java-frontend" module int… (#3452)

--- java-frontend/src/test/files/JavaCheckVerifierFlowsMixedExplicitOrder.java
+++
@@ -1,9 +0,0 @@
-class A {
-
-  // mixed explicit and implicit order is provided
-  void f() {
-    b.toString(); // Noncompliant [[flows=f]] {{error}}  flow@f [[order=3]] {{msg3}}
-    Object a = null; // flow@f [[order=1]] {{msg1}}
-    Object b = new Object();  // flow@f {{msg2}}  -- implicit order, because order attribute is missing
-  }
-}
--- java-frontend/src/test/files/testing/MultipleIssuesSameLine.java
+++
@@ -1,8 +0,0 @@
-class A {
-  // Noncompliant@+2 {{msg 1}}
-  // Noncompliant@+1 {{msg 2}}
-  Object foo;
-  // Noncompliant@+2 {{msg 1}}
-  // Noncompliant@+1 {{msg 2}}
-  Object bar;
-}
