rohan-flutterint/sonar-java

SONARJAVA-4001 Compute the end position of multi-line token only once

java-frontend/src/main/java/org/sonar/java/ast/visitors/SyntaxHighlighterVisitor.java
@@ -40,6 +40,7 @@ import org.sonar.plugins.java.api.tree.AnnotationTree;
 import org.sonar.plugins.java.api.tree.ClassTree;
 import org.sonar.plugins.java.api.tree.Modifier;
 import org.sonar.plugins.java.api.tree.ModifiersTree;
+import org.sonar.plugins.java.api.tree.Range;
 import org.sonar.plugins.java.api.tree.SyntaxToken;
 import org.sonar.plugins.java.api.tree.SyntaxTrivia;
 import org.sonar.plugins.java.api.tree.Tree;
@@ -149,22 +150,11 @@ public class SyntaxHighlighterVisitor extends SubscriptionVisitor {
   }
 
   private void highlight(Tree from, Tree to, TypeOfText typeOfText) {
-    SyntaxToken firstToken = from.firstToken();
-    SyntaxToken lastToken = to.lastToken();
-
-    final int endLine;
-    final int endColumn;
-    if (lastToken.text().startsWith("\"\"\"")) {
-      // slow path for Text Blocks
-      String[] lines = lastToken.text().split("\\r\\n|\\n|\\r");
-      endLine = lastToken.line() + lines.length - 1;
-      endColumn = (lines.length == 1 ? lastToken.column() : 0) + lines[lines.length - 1].length();
-    } else {
-      endLine = lastToken.line();
-      endColumn = lastToken.column() + lastToken.text().length();
-    }
-
-    highlighting.highlight(firstToken.line(), firstToken.column(), endLine, endColumn, typeOfText);
+    Range first = from.firstToken().range();
+    Range last = to.lastToken().range();
+    highlighting.highlight(
+      first.start().line(), first.start().columnOffset(),
+      last.end().line(), last.end().columnOffset(), typeOfText);
   }
 
   @Override
@@ -194,16 +184,11 @@ public class SyntaxHighlighterVisitor extends SubscriptionVisitor {
 
   @Override
   public void visitTrivia(SyntaxTrivia syntaxTrivia) {
-    String comment = syntaxTrivia.comment();
-    int startLine = syntaxTrivia.startLine();
-    int startColumn = syntaxTrivia.column();
-
-    String[] lines = comment.split("\\r\\n|\\n|\\r");
-    int numberLines = lines.length;
-
-    int endLine = startLine + numberLines - 1;
-    int endColumn = numberLines == 1 ? (startColumn + comment.length()) : lines[numberLines - 1].length();
-    boolean isJavadoc = lines[0].trim().startsWith("/**");
-    highlighting.highlight(startLine, startColumn, endLine, endColumn, isJavadoc ? TypeOfText.STRUCTURED_COMMENT : TypeOfText.COMMENT);
+    Range range = syntaxTrivia.range();
+    boolean isJavadoc = syntaxTrivia.comment().startsWith("/**");
+    TypeOfText typeOfText = isJavadoc ? TypeOfText.STRUCTURED_COMMENT : TypeOfText.COMMENT;
+    highlighting.highlight(
+      range.start().line(), range.start().columnOffset(),
+      range.end().line(), range.end().columnOffset(), typeOfText);
   }
 }

java-frontend/src/main/java/org/sonar/java/model/InternalSyntaxTrivia.java
@@ -20,6 +20,8 @@
 package org.sonar.java.model;
 
 import java.util.List;
+import javax.annotation.Nonnull;
+import org.sonar.plugins.java.api.tree.Range;
 import org.sonar.plugins.java.api.tree.SyntaxTrivia;
 import org.sonar.plugins.java.api.tree.Tree;
 import org.sonar.plugins.java.api.tree.TreeVisitor;
@@ -27,13 +29,15 @@ import org.sonar.plugins.java.api.tree.TreeVisitor;
 public class InternalSyntaxTrivia extends JavaTree implements SyntaxTrivia {
 
   private final String comment;
-  private final int startLine;
-  private final int column;
 
-  public InternalSyntaxTrivia(String comment, int startLine, int column) {
+  @Nonnull
+  private final Range range;
+
+  public InternalSyntaxTrivia(String comment, int line, int columnOffset) {
     this.comment = comment;
-    this.startLine = startLine;
-    this.column = column;
+    range = comment.startsWith("/*")
+      ? InternalSyntaxToken.createMultiLineRange(line, columnOffset, comment)
+      : InternalSyntaxToken.createSingleLineRange(line, columnOffset, comment);
   }
 
   @Override
@@ -43,7 +47,7 @@ public class InternalSyntaxTrivia extends JavaTree implements SyntaxTrivia {
 
   @Override
   public int startLine() {
-    return startLine;
+    return range.start().line();
   }
 
   @Override
@@ -72,11 +76,18 @@ public class InternalSyntaxTrivia extends JavaTree implements SyntaxTrivia {
 
   @Override
   public int getLine() {
-    return startLine;
+    return range.start().line();
   }
 
   @Override
   public int column() {
-    return column;
+    return range.start().columnOffset();
   }
+
+  @Nonnull
+  @Override
+  public Range range() {
+    return range;
+  }
+
 }

java-frontend/src/main/java/org/sonar/java/model/JWarning.java
@@ -40,7 +40,7 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.sonar.java.annotations.VisibleForTesting;
 import org.sonar.java.ast.visitors.SubscriptionVisitor;
 import org.sonar.java.collections.SetUtils;
-import org.sonar.plugins.java.api.tree.SyntaxToken;
+import org.sonar.plugins.java.api.tree.Position;
 import org.sonar.plugins.java.api.tree.Tree;
 
 public final class JWarning {
@@ -53,11 +53,11 @@ public final class JWarning {
   private Tree syntaxTree;
 
   @VisibleForTesting
-  JWarning(String message, Type type, int startLine, int startColumn, int endLine, int endColumn) {
+  JWarning(String message, Type type, int startLine, int startColumnOffset, int endLine, int endColumnOffset) {
     this.message = message;
     this.type = type;
-    this.start = Position.exact(startLine, startColumn);
-    this.end = Position.exact(endLine, endColumn);
+    this.start = Position.atOffset(startLine, startColumnOffset);
+    this.end = Position.atOffset(endLine, endColumnOffset);
   }
 
   public Type type() {
@@ -78,48 +78,6 @@ public final class JWarning {
 
   Position end() {
     return end;
-  }
-
-  private static class Position implements Comparable<Position> {
-
-    private static final Comparator<Position> COMPARATOR = Comparator
-      .comparing(Position::line)
-      .thenComparing(Position::column);
-
-    private final int line;
-    private final int column;
-
-    private Position(int line, int column) {
-      this.line = line;
-      this.column = column;
-    }
-
-    private static Position exact(int line, int column) {
-      return new Position(line, column);
-    }
-
-    private static Position startOf(Tree tree) {
-      SyntaxToken token = tree.firstToken();
-      return new Position(token.line(), token.column());
-    }
-
-    private static Position endOf(Tree tree) {
-      SyntaxToken token = tree.lastToken();
-      return new Position(token.line(), token.column() + token.text().length());
-    }
-
-    int line() {
-      return line;
-    }
-
-    int column() {
-      return column;
-    }
-
-    @Override
-    public int compareTo(Position o) {
-      return COMPARATOR.compare(this, o);
-    }
   }
 
   public enum Type {
@@ -232,21 +190,22 @@ public final class JWarning {
         // wrong kind
         return false;
       }
-      return warning.start().compareTo(Position.startOf(tree)) >= 0
-        && warning.end().compareTo(Position.endOf(tree)) <= 0;
+      return warning.start().compareTo(tree.firstToken().range().start()) >= 0
+        && warning.end().compareTo(tree.lastToken().range().end()) <= 0;
     }
 
     @VisibleForTesting
     static boolean isMorePreciseTree(Tree currentTree, Tree newTree) {
-      return Position.startOf(newTree).compareTo( Position.startOf(currentTree)) >= 0
-        && Position.endOf(newTree).compareTo(Position.endOf(currentTree)) <= 0;
+      return newTree.firstToken().range().start().compareTo(currentTree.firstToken().range().start()) >= 0
+        && newTree.lastToken().range().end().compareTo(currentTree.lastToken().range().end()) <= 0;
     }
 
     @VisibleForTesting
     static boolean matchesTreeExactly(JWarning warning) {
       Tree syntaxTree = warning.syntaxTree();
-      return warning.start().compareTo(Position.startOf(syntaxTree)) == 0
-        && warning.end().compareTo(Position.endOf(syntaxTree)) == 0;
+      return warning.start().compareTo(syntaxTree.firstToken().range().start()) == 0
+        && warning.end().compareTo(syntaxTree.lastToken().range().end()) == 0;
     }
   }
+
 }

java-frontend/src/test/java/org/sonar/java/model/InternalSyntaxTriviaTest.java
@@ -1,0 +1,60 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.model;
+
+import org.junit.jupiter.api.Test;
+import org.sonar.plugins.java.api.tree.Range;
+import org.sonar.plugins.java.api.tree.SyntaxTrivia;
+import org.sonar.plugins.java.api.tree.Tree;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InternalSyntaxTriviaTest {
+
+  @Test
+  void single_line_comment() {
+    SyntaxTrivia trivia = InternalSyntaxTrivia.create("// comment", 42, 21);
+    assertThat(trivia.comment()).isEqualTo("// comment");
+    assertThat(trivia.startLine()).isEqualTo(42);
+    assertThat(trivia.column()).isEqualTo(21);
+    assertThat(trivia.range()).isEqualTo(Range.at(42, 22, 42, 32));
+
+    JavaTree tree = (JavaTree) trivia;
+    assertThat(tree.getLine()).isEqualTo(42);
+
+    assertThat(tree.kind()).isEqualTo(Tree.Kind.TRIVIA);
+    assertThat(tree.isLeaf()).isTrue();
+    assertThatThrownBy(tree::children).isInstanceOf(UnsupportedOperationException.class);
+  }
+
+  @Test
+  void multi_line_comment() {
+    SyntaxTrivia trivia = InternalSyntaxTrivia.create("/* line1\n   line2 */", 42, 21);
+    assertThat(trivia.comment()).isEqualTo("/* line1\n   line2 */");
+    assertThat(trivia.startLine()).isEqualTo(42);
+    assertThat(trivia.column()).isEqualTo(21);
+    assertThat(trivia.range()).isEqualTo(Range.at(42, 22, 43, 12));
+
+    JavaTree tree = (JavaTree) trivia;
+    assertThat(tree.getLine()).isEqualTo(42);
+  }
+
+}

java-frontend/src/test/java/org/sonar/java/model/JParserTest.java
@@ -57,6 +57,7 @@ import org.sonar.plugins.java.api.tree.CompilationUnitTree;
 import org.sonar.plugins.java.api.tree.EnumConstantTree;
 import org.sonar.plugins.java.api.tree.ForStatementTree;
 import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.Range;
 import org.sonar.plugins.java.api.tree.Tree;
 import org.sonar.plugins.java.api.tree.TryStatementTree;
 import org.sonar.plugins.java.api.tree.VariableTree;
@@ -201,19 +202,19 @@ class JParserTest {
       CompilationUnitTree t = test("");
       assertEquals("", t.eofToken().text());
       assertEquals(1, t.eofToken().line());
-      assertEquals(0, t.eofToken().column());
+      assertEquals(Range.at(1, 1, 1, 1), t.eofToken().range());
     }
     {
       CompilationUnitTree t = test(" ");
       assertEquals("", t.eofToken().text());
       assertEquals(1, t.eofToken().line());
-      assertEquals(1, t.eofToken().column());
+      assertEquals(Range.at(1, 2, 1, 2), t.eofToken().range());
     }
     {
       CompilationUnitTree t = test(" \n");
       assertEquals("", t.eofToken().text());
       assertEquals(2, t.eofToken().line());
-      assertEquals(0, t.eofToken().column());
+      assertEquals(Range.at(2, 1, 2, 1), t.eofToken().range());
     }
   }
 

java-frontend/src/test/java/org/sonar/java/model/InternalRangeTest.java
@@ -1,0 +1,66 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.model;
+
+import org.junit.jupiter.api.Test;
+import org.sonar.plugins.java.api.tree.Position;
+import org.sonar.plugins.java.api.tree.Range;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InternalRangeTest {
+
+  @Test
+  void construction() {
+    Position start = Position.at(42, 12);
+    Position end = Position.at(43, 17);
+    InternalRange range = new InternalRange(start, end);
+    assertThat(range.start()).isSameAs(start);
+    assertThat(range.end()).isSameAs(end);
+    assertThat(range)
+      .isEqualTo(Range.at(start, end));
+  }
+
+  @Test
+  void to_string() {
+    assertThat(Range.at(Position.at(42, 12), Position.at(43, 17)))
+      .hasToString("(42:12)-(43:17)");
+  }
+
+  @Test
+  void equals() {
+    Position p1 = Position.at(42, 12);
+    Position p2 = Position.at(43, 17);
+    Position p3 = Position.at(44, 1);
+
+    Range range_1_3 = Range.at(p1, p3);
+
+    assertThat(range_1_3.equals(Range.at(p1, p3))).isTrue();
+    assertThat(range_1_3).hasSameHashCodeAs(Range.at(p1, p3));
+
+    assertThat(range_1_3.equals(range_1_3)).isTrue();
+    assertThat(range_1_3.equals(Range.at(p1, p2))).isFalse();
+    assertThat(range_1_3.equals(Range.at(p2, p3))).isFalse();
+    assertThat(range_1_3.equals(null)).isFalse();
+    assertThat(range_1_3.equals(new Object())).isFalse();
+
+  }
+
+}

SonarSource/sonar-java-symbolic-execution

java-frontend/src/main/java/org/sonar/java/ast/visitors/SyntaxHighlighterVisitor.java
@@ -40,6 +40,7 @@ import org.sonar.plugins.java.api.tree.AnnotationTree;
 import org.sonar.plugins.java.api.tree.ClassTree;
 import org.sonar.plugins.java.api.tree.Modifier;
 import org.sonar.plugins.java.api.tree.ModifiersTree;
+import org.sonar.plugins.java.api.location.Range;
 import org.sonar.plugins.java.api.tree.SyntaxToken;
 import org.sonar.plugins.java.api.tree.SyntaxTrivia;
 import org.sonar.plugins.java.api.tree.Tree;
@@ -149,22 +150,11 @@ public class SyntaxHighlighterVisitor extends SubscriptionVisitor {
   }
 
   private void highlight(Tree from, Tree to, TypeOfText typeOfText) {
-    SyntaxToken firstToken = from.firstToken();
-    SyntaxToken lastToken = to.lastToken();
-
-    final int endLine;
-    final int endColumn;
-    if (lastToken.text().startsWith("\"\"\"")) {
-      // slow path for Text Blocks
-      String[] lines = lastToken.text().split("\\r\\n|\\n|\\r");
-      endLine = lastToken.line() + lines.length - 1;
-      endColumn = (lines.length == 1 ? lastToken.column() : 0) + lines[lines.length - 1].length();
-    } else {
-      endLine = lastToken.line();
-      endColumn = lastToken.column() + lastToken.text().length();
-    }
-
-    highlighting.highlight(firstToken.line(), firstToken.column(), endLine, endColumn, typeOfText);
+    Range first = from.firstToken().range();
+    Range last = to.lastToken().range();
+    highlighting.highlight(
+      first.start().line(), first.start().columnOffset(),
+      last.end().line(), last.end().columnOffset(), typeOfText);
   }
 
   @Override
@@ -194,16 +184,11 @@ public class SyntaxHighlighterVisitor extends SubscriptionVisitor {
 
   @Override
   public void visitTrivia(SyntaxTrivia syntaxTrivia) {
-    String comment = syntaxTrivia.comment();
-    int startLine = syntaxTrivia.startLine();
-    int startColumn = syntaxTrivia.column();
-
-    String[] lines = comment.split("\\r\\n|\\n|\\r");
-    int numberLines = lines.length;
-
-    int endLine = startLine + numberLines - 1;
-    int endColumn = numberLines == 1 ? (startColumn + comment.length()) : lines[numberLines - 1].length();
-    boolean isJavadoc = lines[0].trim().startsWith("/**");
-    highlighting.highlight(startLine, startColumn, endLine, endColumn, isJavadoc ? TypeOfText.STRUCTURED_COMMENT : TypeOfText.COMMENT);
+    Range range = syntaxTrivia.range();
+    boolean isJavadoc = syntaxTrivia.comment().startsWith("/**");
+    TypeOfText typeOfText = isJavadoc ? TypeOfText.STRUCTURED_COMMENT : TypeOfText.COMMENT;
+    highlighting.highlight(
+      range.start().line(), range.start().columnOffset(),
+      range.end().line(), range.end().columnOffset(), typeOfText);
   }
 }

java-frontend/src/main/java/org/sonar/java/model/InternalSyntaxTrivia.java
@@ -20,6 +20,8 @@
 package org.sonar.java.model;
 
 import java.util.List;
+import javax.annotation.Nonnull;
+import org.sonar.plugins.java.api.location.Range;
 import org.sonar.plugins.java.api.tree.SyntaxTrivia;
 import org.sonar.plugins.java.api.tree.Tree;
 import org.sonar.plugins.java.api.tree.TreeVisitor;
@@ -27,13 +29,15 @@ import org.sonar.plugins.java.api.tree.TreeVisitor;
 public class InternalSyntaxTrivia extends JavaTree implements SyntaxTrivia {
 
   private final String comment;
-  private final int startLine;
-  private final int column;
 
-  public InternalSyntaxTrivia(String comment, int startLine, int column) {
+  @Nonnull
+  private final Range range;
+
+  public InternalSyntaxTrivia(String comment, int line, int columnOffset) {
     this.comment = comment;
-    this.startLine = startLine;
-    this.column = column;
+    range = comment.startsWith("/*")
+      ? InternalSyntaxToken.createMultiLineRange(line, columnOffset, comment)
+      : InternalSyntaxToken.createSingleLineRange(line, columnOffset, comment);
   }
 
   @Override
@@ -43,7 +47,7 @@ public class InternalSyntaxTrivia extends JavaTree implements SyntaxTrivia {
 
   @Override
   public int startLine() {
-    return startLine;
+    return range.start().line();
   }
 
   @Override
@@ -72,11 +76,18 @@ public class InternalSyntaxTrivia extends JavaTree implements SyntaxTrivia {
 
   @Override
   public int getLine() {
-    return startLine;
+    return range.start().line();
   }
 
   @Override
   public int column() {
-    return column;
+    return range.start().columnOffset();
   }
+
+  @Nonnull
+  @Override
+  public Range range() {
+    return range;
+  }
+
 }

java-frontend/src/main/java/org/sonar/java/model/JWarning.java
@@ -40,7 +40,7 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.sonar.java.annotations.VisibleForTesting;
 import org.sonar.java.ast.visitors.SubscriptionVisitor;
 import org.sonar.java.collections.SetUtils;
-import org.sonar.plugins.java.api.tree.SyntaxToken;
+import org.sonar.plugins.java.api.location.Position;
 import org.sonar.plugins.java.api.tree.Tree;
 
 public final class JWarning {
@@ -53,11 +53,11 @@ public final class JWarning {
   private Tree syntaxTree;
 
   @VisibleForTesting
-  JWarning(String message, Type type, int startLine, int startColumn, int endLine, int endColumn) {
+  JWarning(String message, Type type, int startLine, int startColumnOffset, int endLine, int endColumnOffset) {
     this.message = message;
     this.type = type;
-    this.start = Position.exact(startLine, startColumn);
-    this.end = Position.exact(endLine, endColumn);
+    this.start = Position.atOffset(startLine, startColumnOffset);
+    this.end = Position.atOffset(endLine, endColumnOffset);
   }
 
   public Type type() {
@@ -78,48 +78,6 @@ public final class JWarning {
 
   Position end() {
     return end;
-  }
-
-  private static class Position implements Comparable<Position> {
-
-    private static final Comparator<Position> COMPARATOR = Comparator
-      .comparing(Position::line)
-      .thenComparing(Position::column);
-
-    private final int line;
-    private final int column;
-
-    private Position(int line, int column) {
-      this.line = line;
-      this.column = column;
-    }
-
-    private static Position exact(int line, int column) {
-      return new Position(line, column);
-    }
-
-    private static Position startOf(Tree tree) {
-      SyntaxToken token = tree.firstToken();
-      return new Position(token.line(), token.column());
-    }
-
-    private static Position endOf(Tree tree) {
-      SyntaxToken token = tree.lastToken();
-      return new Position(token.line(), token.column() + token.text().length());
-    }
-
-    int line() {
-      return line;
-    }
-
-    int column() {
-      return column;
-    }
-
-    @Override
-    public int compareTo(Position o) {
-      return COMPARATOR.compare(this, o);
-    }
   }
 
   public enum Type {
@@ -232,21 +190,22 @@ public final class JWarning {
         // wrong kind
         return false;
       }
-      return warning.start().compareTo(Position.startOf(tree)) >= 0
-        && warning.end().compareTo(Position.endOf(tree)) <= 0;
+      return warning.start().compareTo(tree.firstToken().range().start()) >= 0
+        && warning.end().compareTo(tree.lastToken().range().end()) <= 0;
     }
 
     @VisibleForTesting
     static boolean isMorePreciseTree(Tree currentTree, Tree newTree) {
-      return Position.startOf(newTree).compareTo( Position.startOf(currentTree)) >= 0
-        && Position.endOf(newTree).compareTo(Position.endOf(currentTree)) <= 0;
+      return newTree.firstToken().range().start().compareTo(currentTree.firstToken().range().start()) >= 0
+        && newTree.lastToken().range().end().compareTo(currentTree.lastToken().range().end()) <= 0;
     }
 
     @VisibleForTesting
     static boolean matchesTreeExactly(JWarning warning) {
       Tree syntaxTree = warning.syntaxTree();
-      return warning.start().compareTo(Position.startOf(syntaxTree)) == 0
-        && warning.end().compareTo(Position.endOf(syntaxTree)) == 0;
+      return warning.start().compareTo(syntaxTree.firstToken().range().start()) == 0
+        && warning.end().compareTo(syntaxTree.lastToken().range().end()) == 0;
     }
   }
+
 }

java-frontend/src/test/java/org/sonar/java/model/InternalSyntaxTriviaTest.java
@@ -1,0 +1,60 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.model;
+
+import org.junit.jupiter.api.Test;
+import org.sonar.plugins.java.api.location.Range;
+import org.sonar.plugins.java.api.tree.SyntaxTrivia;
+import org.sonar.plugins.java.api.tree.Tree;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InternalSyntaxTriviaTest {
+
+  @Test
+  void single_line_comment() {
+    SyntaxTrivia trivia = InternalSyntaxTrivia.create("// comment", 42, 21);
+    assertThat(trivia.comment()).isEqualTo("// comment");
+    assertThat(trivia.startLine()).isEqualTo(42);
+    assertThat(trivia.column()).isEqualTo(21);
+    assertThat(trivia.range()).isEqualTo(Range.at(42, 22, 42, 32));
+
+    JavaTree tree = (JavaTree) trivia;
+    assertThat(tree.getLine()).isEqualTo(42);
+
+    assertThat(tree.kind()).isEqualTo(Tree.Kind.TRIVIA);
+    assertThat(tree.isLeaf()).isTrue();
+    assertThatThrownBy(tree::children).isInstanceOf(UnsupportedOperationException.class);
+  }
+
+  @Test
+  void multi_line_comment() {
+    SyntaxTrivia trivia = InternalSyntaxTrivia.create("/* line1\n   line2 */", 42, 21);
+    assertThat(trivia.comment()).isEqualTo("/* line1\n   line2 */");
+    assertThat(trivia.startLine()).isEqualTo(42);
+    assertThat(trivia.column()).isEqualTo(21);
+    assertThat(trivia.range()).isEqualTo(Range.at(42, 22, 43, 12));
+
+    JavaTree tree = (JavaTree) trivia;
+    assertThat(tree.getLine()).isEqualTo(42);
+  }
+
+}

java-frontend/src/test/java/org/sonar/java/model/JParserTest.java
@@ -57,6 +57,7 @@ import org.sonar.plugins.java.api.tree.CompilationUnitTree;
 import org.sonar.plugins.java.api.tree.EnumConstantTree;
 import org.sonar.plugins.java.api.tree.ForStatementTree;
 import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.location.Range;
 import org.sonar.plugins.java.api.tree.Tree;
 import org.sonar.plugins.java.api.tree.TryStatementTree;
 import org.sonar.plugins.java.api.tree.VariableTree;
@@ -201,19 +202,19 @@ class JParserTest {
       CompilationUnitTree t = test("");
       assertEquals("", t.eofToken().text());
       assertEquals(1, t.eofToken().line());
-      assertEquals(0, t.eofToken().column());
+      assertEquals(Range.at(1, 1, 1, 1), t.eofToken().range());
     }
     {
       CompilationUnitTree t = test(" ");
       assertEquals("", t.eofToken().text());
       assertEquals(1, t.eofToken().line());
-      assertEquals(1, t.eofToken().column());
+      assertEquals(Range.at(1, 2, 1, 2), t.eofToken().range());
     }
     {
       CompilationUnitTree t = test(" \n");
       assertEquals("", t.eofToken().text());
       assertEquals(2, t.eofToken().line());
-      assertEquals(0, t.eofToken().column());
+      assertEquals(Range.at(2, 1, 2, 1), t.eofToken().range());
     }
   }
 

java-frontend/src/test/java/org/sonar/java/model/location/InternalRangeTest.java
@@ -1,0 +1,66 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.model.location;
+
+import org.junit.jupiter.api.Test;
+import org.sonar.plugins.java.api.location.Position;
+import org.sonar.plugins.java.api.location.Range;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InternalRangeTest {
+
+  @Test
+  void construction() {
+    Position start = Position.at(42, 12);
+    Position end = Position.at(43, 17);
+    InternalRange range = new InternalRange(start, end);
+    assertThat(range.start()).isSameAs(start);
+    assertThat(range.end()).isSameAs(end);
+    assertThat(range)
+      .isEqualTo(Range.at(start, end));
+  }
+
+  @Test
+  void to_string() {
+    assertThat(Range.at(Position.at(42, 12), Position.at(43, 17)))
+      .hasToString("(42:12)-(43:17)");
+  }
+
+  @Test
+  void equals() {
+    Position p1 = Position.at(42, 12);
+    Position p2 = Position.at(43, 17);
+    Position p3 = Position.at(44, 1);
+
+    Range range_1_3 = Range.at(p1, p3);
+
+    assertThat(range_1_3.equals(Range.at(p1, p3))).isTrue();
+    assertThat(range_1_3).hasSameHashCodeAs(Range.at(p1, p3));
+
+    assertThat(range_1_3.equals(range_1_3)).isTrue();
+    assertThat(range_1_3.equals(Range.at(p1, p2))).isFalse();
+    assertThat(range_1_3.equals(Range.at(p2, p3))).isFalse();
+    assertThat(range_1_3.equals(null)).isFalse();
+    assertThat(range_1_3.equals(new Object())).isFalse();
+
+  }
+
+}