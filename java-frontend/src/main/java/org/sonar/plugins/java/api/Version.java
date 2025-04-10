51dd020d270eb4a530f038ff07515d1600b68b1a
Romain Brenguier <romain.brenguier@sonarsource.com>
2025-04-10 12:05:55 +0200

    SONARJAVA-5383 Lookup Spring-web dependency version to filter out Spring specific rule (#5053)
    Co-authored-by: Leonardo Pilastri <leonardo.pilastri@sonarsource.com>
    Improves the SpringComposedRequestMappingCheck. 
    Introduces DependencyVersionAware API.
    Provides a simple example of the usage of the new API.

---
+++ java-frontend/src/main/java/org/sonar/plugins/java/api/Version.java
@@ -0,0 +1,35 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.plugins.java.api;
+
+/** Versions of libraries. This provides methods to compare the version with other ones. */
+public interface Version {
+
+  Integer major();
+  Integer minor();
+  Integer patch();
+  String qualifier();
+
+  boolean isGreaterThanOrEqualTo(Version version);
+  boolean isGreaterThanOrEqualTo(String version);
+  boolean isGreaterThan(Version version);
+  boolean isLowerThanOrEqualTo(Version version);
+  boolean isLowerThan(Version version);
+  boolean isGreaterThan(String version);
+  boolean isLowerThanOrEqualTo(String version);
+  boolean isLowerThan(String version);
+}
