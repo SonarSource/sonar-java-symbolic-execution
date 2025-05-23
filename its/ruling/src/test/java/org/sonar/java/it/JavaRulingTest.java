e40d6722cde9dc6a5f735b75edb42bb876558b55
Leonardo Pilastri <leonardo.pilastri@sonarsource.com>
2025-05-23 16:20:32 +0200

    Add sonar.java.version property to specify sonar-java-plugin to install in ruling test

--- its/ruling/src/test/java/org/sonar/java/it/JavaRulingTest.java
+++ its/ruling/src/test/java/org/sonar/java/it/JavaRulingTest.java
@@ -75,7 +75,7 @@ public class JavaRulingTest {
     OrchestratorRuleBuilder orchestratorBuilder = OrchestratorRule.builderEnv()
       .useDefaultAdminCredentialsForBuilds(true)
       .setSonarVersion(System.getProperty("sonar.runtimeVersion", "LATEST_RELEASE"))
-      .addPlugin(MavenLocation.of("org.sonarsource.java", "sonar-java-plugin", "DEV"))
+      .addPlugin(MavenLocation.of("org.sonarsource.java", "sonar-java-plugin", System.getProperty("sonar.java.version", "DEV")))
       .addPlugin(FileLocation.of(TestClasspathUtils.findModuleJarPath("../../java-symbolic-execution/java-symbolic-execution-plugin").toFile()))
       .addPlugin(MavenLocation.of("org.sonarsource.sonar-lits-plugin", "sonar-lits-plugin", "0.11.0.2659"));
 
