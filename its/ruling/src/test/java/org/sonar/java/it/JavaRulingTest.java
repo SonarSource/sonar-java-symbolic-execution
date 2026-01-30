d0299592b0f2539f8cb88bfb44e014ef6341c620
romain.birling <romain.birling@sonarsource.com>
2026-01-30 14:15:05 +0100

    Try to fix 4

--- its/ruling/src/test/java/org/sonar/java/it/JavaRulingTest.java
+++ its/ruling/src/test/java/org/sonar/java/it/JavaRulingTest.java
@@ -132,7 +132,7 @@ public class JavaRulingTest {
     MavenBuild build = test_project(projectKey, projectName);
     build
       // by default guava is compatible with java 6, however this is not supported with JDK 23
-      .setProperty("java.version", "1.8")
+      .setProperty("java.version", "23")
       .setProperty("maven.javadoc.skip", "true")
       // use batch
       .setProperty("sonar.java.experimental.batchModeSizeInKB", "8192");
