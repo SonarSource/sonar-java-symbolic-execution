5caba5d43a8af9befc074c7d751cfc4869fa896a
Mary Georgiou <89914005+mary-georgiou@users.noreply.github.com>
2026-08-13 13:24:34 +0200

    JAVASE-240 Fix guava ruling IT failing under SonarQube's Java 21 scanner requirement (#125)

--- its/ruling/src/test/java/org/sonar/java/it/JavaRulingTest.java
+++ its/ruling/src/test/java/org/sonar/java/it/JavaRulingTest.java
@@ -131,8 +131,6 @@ public class JavaRulingTest {
     String projectKey = "com.google.guava:guava";
     MavenBuild build = test_project(projectKey, projectName);
     build
-      // by default guava is compatible with java 6, however this is not supported with JDK 17+
-      .setProperty("java.version", "1.8")
       .setProperty("maven.javadoc.skip", "true")
       // use batch
       .setProperty("sonar.java.experimental.batchModeSizeInKB", "8192");
